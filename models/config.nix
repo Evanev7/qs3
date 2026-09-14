ee589b903bb2224405a181f95f765e810497d9bb
Evan <evanev7@gmail.com>
2026-09-14 19:02:59 +0100

    q3.8-27b model

--- models/config.nix
+++ models/config.nix
@@ -23,6 +23,7 @@ let
   models = {
     "qwen3.6-35b-a3b" = readModel ./qwen3.6-35b-a3b;
     "qwen3.6-27b" = readModel ./qwen3.6-27b;
+    "qwen3.8-27b" = readModel ./qwen3.8-27b;
   };
   model = models.${engine.model};
   text = model.config;
---
+++ models/qwen3.8-27b/source.nix
@@ -0,0 +1,4 @@
+{
+  repo = "Qwen/Qwen3.8-27B";
+  rev = "1d4bf0f2ff6012fd82039f2fa52739d0dd7c60c0";
+}
